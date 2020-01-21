a0faf088715d80eb2bec2ef5a25beb713b476cf1
IbanJ <iban6698@hotmail.com>
2020-01-21 22:55:47 +0100

    Funciona el form pero no sube datos y tampoco hemos añadido los CKBOX

--- ProyectoAgencia/ProyectoAgencia/Program.cs
+++ ProyectoAgencia/ProyectoAgencia/Program.cs
@@ -16,7 +16,7 @@ namespace ProyectoAgencia
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FormFestivos());
         }
     }
 }
